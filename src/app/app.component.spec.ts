aaee390584d82506c0a049d5a8a1f7682ab0d7d1
bharath <naveenbharath93@gmail.com>
2019-04-23 20:52:09 +0530

    test case updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,65 @@
-import { TestBed, ComponentFixture, async } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
 import { By } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
 
-let component: AppComponent;
-let fixture: ComponentFixture<AppComponent>;
+import { AppComponent } from "./app.component";
+import { FilterPipe } from './pipes/filter.pipe';
 
 describe("AppComponent", () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
-    });
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let filter: FilterPipe;
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [FormsModule],
+        declarations: [AppComponent, FilterPipe]
+      }).compileComponents();
+
+      filter = new FilterPipe();
+    })
+  );
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
 
   it("file change event should arrive in handler", () => {
-    let input = fixture.debugElement.query(By.css("input[type=file]"))
-      .nativeElement;
+    let input = fixture.debugElement.query(By.css("input[type=file]")).nativeElement;
     spyOn(component, "uploadFile");
     input.dispatchEvent(new Event("change"));
     expect(component.uploadFile).toHaveBeenCalled();
+  });
+
+  it("filter pipe should be instanciated", () => {
+    expect(FilterPipe).toBeDefined();
+  });
+
+  it("filter pipe should return items if no field is given", () => {
+    let items = [];
+    items.push({ id: 1, ['Issue count']: '5' });
+
+    let filtered = filter.transform(items, null, "Issue count");
+    expect(filtered).toEqual(items);
+  });
+
+  it("filter pipe should filter", () => {
+    let items = [];
+
+    items.push({ ['Issue count']: '5' });
+    items.push({ ['Issue count']: '7' });
+    items.push({ ['Issue count']: '7' });
+    items.push({ ['Issue count']: '5' });
+
+    let filtered = filter.transform(items, '7', "Issue count");
+
+    expect(filtered.length).toBe(2);
   });
 });
